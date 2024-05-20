837e0d5cfa2ccefb3623babdfea8aa9f3301a5aa
pridestalkerr <danartenie158@gmail.com>
2024-05-20 10:13:42 +0300

    fix for trpc 11

--- src/utils/procedure.ts
+++ src/utils/procedure.ts
@@ -29,6 +29,14 @@ const getProcedureType = (procedure: OpenApiProcedure): ProcedureType => {
   if (procedure._def.query) return 'query';
   if (procedure._def.mutation) return 'mutation';
   if (procedure._def.subscription) return 'subscription';
+
+  if (procedure._def.type === 'query')
+    return 'query';
+  if (procedure._def.type === 'mutation')
+    return 'mutation';
+  if (procedure._def.type === 'subscription')
+    return 'subscription';
+
   throw new Error('Unknown procedure type');
 };
 
